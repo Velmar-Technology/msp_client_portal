d2f75b9a78686e4a4c900a4335730f5e6d3014d4
eapolanco <eapolancorobles@outlook.com>
2026-06-20 22:21:12 -0400

    fix: Uncaught SyntaxError: The requested module does not provide an export named 'ClientError'

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -10,6 +10,9 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   },
+  optimizeDeps: {
+    include: ['@shared/errors'],
+  },
   server: {
     port: 5173,
     proxy: {
